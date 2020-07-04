99d9cd4de102dc6756e1dc9d2203c243ed485fe7
Kiselev-Minaev <hope.anton@gmail.com>
2020-07-04 08:10:02 +0300

    Add fragment matcher

--- src/apolloClient.tsx
+++ src/apolloClient.tsx
@@ -1,10 +1,21 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import { IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
+import introspectionResult from '../fragmentTypes.json';
+
+const fragmentMatcher = new IntrospectionFragmentMatcher({
+  introspectionQueryResultData: introspectionResult,
+});
+
+const cache = new InMemoryCache({
+  fragmentMatcher,
+});
 
 const apolloClient = new ApolloClient({
   uri: 'https://api.github.com/graphql',
   headers: {
     Authorization: `bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
   },
+  cache,
 });
 
 export default apolloClient;
