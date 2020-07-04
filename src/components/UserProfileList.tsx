9df286a9af5a3cb840e315153d5837eefc4243b2
Kiselev-Minaev <hope.anton@gmail.com>
2020-07-04 18:54:42 +0300

    Handle user not found case and add testing lib

--- src/components/UserProfileList.tsx
+++ src/components/UserProfileList.tsx
@@ -4,13 +4,17 @@ import { Search_search_edges } from '../generated/apollo/Search';
 
 const UserProfileList: React.FC<{
   profiles: (Search_search_edges | null)[];
-}> = ({ profiles }) => (
-  <>
-    {profiles &&
-      profiles.map((profile, index) => (
-        <UserProfile key={index} profile={profile!.node!} />
-      ))}
-  </>
-);
+}> = ({ profiles }) => {
+  if (profiles.length === 0)
+    return <>We couldn’t find any users matching your request</>;
+  return (
+    <>
+      {profiles &&
+        profiles.map((profile, index) => (
+          <UserProfile key={index} profile={profile!.node!} />
+        ))}
+    </>
+  );
+};
 
 export default UserProfileList;
