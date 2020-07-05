99c7476846fe875d74af0d06f04539d460700849
Kiselev-Minaev <hope.anton@gmail.com>
2020-07-05 08:41:20 +0300

    Add user profile list tests

--- src/components/UserProfileList.test.tsx
+++ src/components/UserProfileList.test.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import UserProfileList from './UserProfileList';
+import { Search_search_edges } from '../generated/apollo/Search';
 
 describe('UserProfileList', () => {
-  it('should render the country info', async () => {
-    const { getByText } = render(<UserProfileList profiles={[]} />);
-    getByText(/We couldn’t/i);
+  let profilesMock: (Search_search_edges | null)[];
+
+  describe('given no profiles', () => {
+    it("renders 'we couldn’t find' text", async () => {
+      const { getByText } = render(<UserProfileList profiles={[]} />);
+      getByText(/We couldn’t find/i);
+    });
+  });
+
+  describe('given one profile', () => {
+    profilesMock = [
+      {
+        __typename: 'SearchResultItemEdge',
+        node: {
+          avatarUrl: '',
+          email: '',
+          login: 'some-login',
+          url: 'https://some-url',
+          __typename: 'User',
+          repositories: {
+            __typename: 'RepositoryConnection',
+            nodes: [
+              {
+                description: 'some-descr',
+                url: 'https://some-url',
+                name: 'some-name',
+                __typename: 'Repository',
+              },
+            ],
+          },
+        },
+      },
+    ];
+    it("renders user's login", () => {
+      const { getByText } = render(<UserProfileList profiles={profilesMock} />);
+      getByText(/some-login/);
+    });
   });
 });
